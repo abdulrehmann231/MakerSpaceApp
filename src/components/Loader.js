e3499f708799baf2cf40d644bd5b10752263f9d6
abdulrehmann231 <mabdulrehman951@gmail.com>
2025-09-05 23:17:11 +0500

    fix: loader position

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -2,8 +2,8 @@
 
 export default function Loader() {
   return (
-    <div id="loader" className="loader justify-content-center">
-      <div className="maxui-roller align-self-center">
+    <div id="loader" className="loader top-0 d-flex justify-content-center align-items-center">
+      <div className="maxui-roller">
         <div></div>
         <div></div>
         <div></div>
